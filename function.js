f18160881de1c727b1905108e05ca93fd835b869
NoorRahman1234 <rahmanafr0123@gmail.com>
2026-02-17 14:44:12 +0500

    let const and data types

---
+++ function.js
@@ -0,0 +1,23 @@
+// console.log("Hi Noor");
+// document.write("Hi Noor..")
+
+// let a = "yahoo-baba";
+// document.write(a);
+
+// var x=25;
+
+// var x =true;
+
+// var x=["html","Css","js"];
+
+// var x={ 
+//     fname:"Ali",
+//     lname:"Khan",
+//     age:"23"
+// }
+// console.log(x ,typeof x);
+ 
+// var x= null;
+
+var x;
+console.log(x);
