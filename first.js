1250733b03b492fca87e51bc51a28530c1850827
NoorRahman1234 <rahmanafr0123@gmail.com>
2026-02-18 11:01:01 +0500

    make first.js file

---
+++ first.js
@@ -0,0 +1,13 @@
+ // Array
+ // Array created using literal notation
+  // const fruits = ["Apple", "Mango"]
+  // console.log(fruits.length);
+  /////////////////////////////////////////////////////////////////////////////////////
+// array created using the array() constractor.
+// const fruits2 = new Array("Peach", "Banana", "Strabery");
+// console.log(fruits2.length);
+
+/////////////////////////////////////////////////////////////////////////////////////
+// Array created using String.prototype.split()
+// const fruits3 = "Apple, Banana".split(", ");
+// console.log(fruits3.length);
