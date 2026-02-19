be512d0879fa3780d457200aad9a5bd61e71dc52
NoorRahman1234 <rahmanafr0123@gmail.com>
2026-02-19 15:22:00 +0500

    Added callbackfunction

---
+++ callback-function.js
@@ -0,0 +1,13 @@
+
+
+// Callback function
+// function greet(name){
+//     console.log("Hello " + name);
+// }
+// function processorUserInput(callback){
+//     const name = "Noor";
+//     callback(name);
+// }
+// processorUserInput(greet);
+
+// Output will be Hello..Noor
