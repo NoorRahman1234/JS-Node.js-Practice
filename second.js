538cf412625efa0f34d899384ce5eb88b11141ff
NoorRahman1234 <rahmanafr0123@gmail.com>
2026-02-19 11:05:18 +0500

    Add Object Destructuring

--- second.js
+++ second.js
@@ -28,11 +28,23 @@
 // const JsUser = {
 //     name: "Noor",
 //     age: "22",
-//     symbol: "Mykey1",
+//     [symbol]: "Mykey1",
 //     location: "Peshawar",
 //     email: "rahmanafr@gmail.com",
 //     isLoggedIn: "false",
 //     lastLoginDays: ["Monday", "Saturday"]
 // }
-// console.log(JsUser.symbol);
+// console.log(JsUser);
 // console.log(JsUser["email"]);
+
+/////////////////////////////////////////////////////
+// Object Destructuring 
+// Destructuring means Extracting any value from an object
+// const course ={
+//     coursename: "IT",
+//     price: "20000",
+//     courseinstructor: "Teacher"
+
+// }
+// const {coursename, price} = course
+// console.log(coursename, price);
