a25c1b3447e8068c8d111785a4d61f7a3ab4b36b
NoorRahman1234 <rahmanafr0123@gmail.com>
2026-02-19 09:49:02 +0500

    Add Object exmaples

--- second.js
+++ second.js
@@ -5,4 +5,32 @@
 //    age: "22",
 //    add: "Peshawar"
 // };
+
+
+// for (let key in student) {
+//    console.log(`${key} : ${student[key]}`);
+// }
 // console.log(student);
+// It will print all the keys and values in the object
+///////////////////////////////////////////////////////////////////
+// if we want to print one key and its values so we can do like this
+// const student1 ={
+//    name: "Noor",
+//    age: "22",
+//    add: "Peshawar"
+// };
+// console.log("name :" + student1.name);
+
+
+// Example No 3
+// Accessing object using different console 
+// const JsUser = {
+//     name: "Noor",
+//     age: "22",
+//     location: "Peshawar",
+//     email: "rahmanafr@gmail.com",
+//     isLoggedIn: "false",
+//     lastLoginDays: ["Monday", "Saturday"]
+// }
+// console.log(JsUser.email);
+// console.log(JsUser["email"]);
