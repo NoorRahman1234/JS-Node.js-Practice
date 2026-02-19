d37eae7eb51aece9633130dbb08bf4897e5fb1e3
NoorRahman1234 <rahmanafr0123@gmail.com>
2026-02-19 15:23:48 +0500

    second example of callback function

--- callback-function.js
+++ callback-function.js
@@ -11,3 +11,16 @@
 // processorUserInput(greet);
 
 // Output will be Hello..Noor
+
+
+// Example No 2
+function first() {
+    console.log("First function");
+}
+
+function second(callback) {
+    console.log("Second function");
+    callback();
+}
+
+second(first);
