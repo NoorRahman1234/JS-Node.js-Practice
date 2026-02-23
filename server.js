5ae3842472564417a1be603e49512690d63b5953
NoorRahman1234 <rahmanafr0123@gmail.com>
2026-02-23 11:57:31 +0500

    Make a first node js server

---
+++ server.js
@@ -0,0 +1,28 @@
+
+// First Server 
+// const http = require("http");
+
+// const server = http.createServer((req, res) =>{
+//     console.log("Request received");
+//     res.write("Hello Noor first node js server is running!");
+//     res.end();
+// });
+// server.listen(3000, () =>{
+//     console.log("server is running on port 3000");
+// });
+
+
+
+// Example No 2
+const http = require("http");
+const  server = http.createServer((req, res) =>{
+    if(req.url === "/"){
+        res.end("Home Page");        
+    } else if(req.url === "/about"){
+        res.end("About Page");
+        
+    }else{
+        res.end("404 Not found");
+    }
+});
+server.listen(4000);
