17628bf63328532baa9f4dde06d86528bf65c435
NoorRahman1234 <rahmanafr0123@gmail.com>
2026-02-23 14:36:27 +0500

    make some new changes

--- server.js
+++ server.js
@@ -14,15 +14,15 @@
 
 
 // Example No 2
-const http = require("http");
-const  server = http.createServer((req, res) =>{
-    if(req.url === "/"){
-        res.end("Home Page");        
-    } else if(req.url === "/about"){
-        res.end("About Page");
+// const http = require("http");
+// const  server = http.createServer((req, res) =>{
+//     if(req.url === "/"){
+//         res.end("Home Page");        
+//     } else if(req.url === "/about"){
+//         res.end("About Page");
         
-    }else{
-        res.end("404 Not found");
-    }
-});
-server.listen(4000);
+//     }else{
+//         res.end("404 Not found");
+//     }
+// });
+// server.listen(4000);
