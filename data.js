77010e7271a4f0d48d4d4d051782452896f787bf
NoorRahman1234 <rahmanafr0123@gmail.com>
2026-02-24 12:28:55 +0500

    File System in Node.js  read and wirte

---
+++ data.js
@@ -0,0 +1,1 @@
+Hello world Noor rahman12
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 
 // First Server 
+
 // const http = require("http");
 
 // const server = http.createServer((req, res) =>{
@@ -14,6 +15,7 @@
 
 
 // Example No 2
+// URLs
 // const http = require("http");
 // const  server = http.createServer((req, res) =>{
 //     if(req.url === "/"){
@@ -26,3 +28,40 @@
 //     }
 // });
 // server.listen(4000);
+/////////////////////////////////////////////////////////////////
+
+// File System in Node.js
+// Reading file  
+// Reading means getting data from a file
+// Example  Asynchronous Read (Recommended)
+// const fs = require('fs');
+// fs.readFile('F:/JS-Node.js-Practice/first.js', 'utf8', (err, data) =>{
+//     if (err){
+//         console.log ("Error occurred...");
+//         return;
+//     };
+//     console.log(data);
+// });
+
+// Output show all the data inside first.js file and show on the terminal 
+// because I gave first.js file path in the above code for reading a file.
+///////////////////////////////////////////////////////////////////////////
+
+// Sychronous Read(Blocking)
+//  const fs = require('fs');
+//  const data = fs.readFileSync('F:/JS-Node.js-Practice/server.js', 'utf8');
+//  console.log(data);
+
+ // Output will show the same file data because I gave server.js file path 
+
+///////////////////////////////////////////////////////////////////////////
+
+// File system 
+// wirting a File
+// writing means putting data inside a file.
+// it create a file a well by the name of data.js
+const fs = require('fs');
+fs.writeFile('data.js', 'Hello world Noor rahman12', (err) =>{
+    if (err) throw err;
+    console.log("file created successfully");
+})
